5b703d68e00d92ee7f24d616ecfcede5b316e3d5
awaiting-fallback <hdubeux@proton.me>
2025-05-09 17:47:55 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Sander from './Sander.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Sander />
+  </React.StrictMode>,
+)
